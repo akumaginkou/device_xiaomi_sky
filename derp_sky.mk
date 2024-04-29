b4d7e1793eac01b6e875ff5e7be9e7a76be74778
Allan DSouza <allan@allan.co.in>
2024-04-29 10:47:50 +0300

    adapt los tree for derp

--- derp_sky.mk
+++ derp_sky.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common Derp configurations
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -21,9 +21,15 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
+# Derpfest
+DERP_BUILDTYPE := Official
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_6 := true
+TARGET_USES_PICO_GAPPS := true
+
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := lineage_sky
+PRODUCT_NAME := derp_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
